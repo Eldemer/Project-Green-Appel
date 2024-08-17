4aac53d42ff5b26f716595749c1b006cc595c25c
ahmedeldemer <ahmedeldemer88@gmail.com>
2024-08-17 04:44:35 +0300

    Last V

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,26 +10,26 @@
 	<title>Green Apple</title>
 
 	<!-- favicon -->
-	<link rel="shortcut icon" type="image/png" href="assets/img/favicon.png">
+	<link rel="shortcut icon" type="image/png" href="{{ asset('assets/img/favicon.png1') }}">
 	<!-- google font -->
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet">
 	<!-- fontawesome -->
-	<link rel="stylesheet" href="assets/css/all.min.css">
+	<link rel="stylesheet" href="{{ asset('assets/css/all.min.css') }}">
 	<!-- bootstrap -->
-	<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}">
 	<!-- owl carousel -->
-	<link rel="stylesheet" href="assets/css/owl.carousel.css">
+	<link rel="stylesheet" href="{{ asset('assets/css/owl.carousel.css') }}">
 	<!-- magnific popup -->
-	<link rel="stylesheet" href="assets/css/magnific-popup.css">
+	<link rel="stylesheet" href="{{ asset('assets/css/magnific-popup.css') }}">
 	<!-- animate css -->
-	<link rel="stylesheet" href="assets/css/animate.css">
+	<link rel="stylesheet" href="{{ asset('assets/css/animate.css') }}">
 	<!-- mean menu css -->
-	<link rel="stylesheet" href="assets/css/meanmenu.min.css">
+	<link rel="stylesheet" href="{{ asset('assets/css/meanmenu.min.css') }}">
 	<!-- main style -->
-	<link rel="stylesheet" href="assets/css/main.css">
+	<link rel="stylesheet" href="{{ asset('assets/css/main.css') }}">
 	<!-- responsive -->
-	<link rel="stylesheet" href="assets/css/responsive.css">
+	<link rel="stylesheet" href="{{ asset('assets/css/responsive.css') }}">
 
 </head>
 <body>
@@ -50,8 +50,8 @@
 					<div class="main-menu-wrap">
 						<!-- logo -->
 						<div class="site-logo">
-							<a href="index.html">
-								<img src="assets/img/logo.png" alt="">
+							<a href="{{ asset('index.html') }}">
+								<img src="{{ asset('assets/img/logo.png') }}" alt="">
 							</a>
 						</div>
 						<!-- logo -->
@@ -59,25 +59,25 @@
 						<!-- menu start -->
 						<nav class="main-menu">
 							<ul>
-								<li class="current-list-item"><a href="\">Home</a>
+								<li class="current-list-item"><a href="{{ asset('/') }}">Home</a>
 
 								</li>
-								<li><a href="/product">Products</a></li>
+								<li><a href="{{ asset('/product') }}">Products</a></li>
 
-								<li><a href="/about">About</a>
+								<li><a href="{{ asset('/about') }}">About</a>
 
 								</li>
-								<li><a href="/contact">Contact</a></li>
+								<li><a href="{{ asset('/contact') }}">Contact</a></li>
 
 								<li>
 									<div class="header-icons">
-										<a class="shopping-cart" href="cart.html"><i class="fas fa-shopping-cart"></i></a>
-										<a class="mobile-hide search-bar-icon" href="#"><i class="fas fa-search"></i></a>
+										<a class="shopping-cart" href="{{ asset('cart.html') }}"><i class="fas fa-shopping-cart"></i></a>
+										<a class="mobile-hide search-bar-icon" href="{{ asset('#') }}"><i class="fas fa-search"></i></a>
 									</div>
 								</li>
 							</ul>
 						</nav>
-						<a class="mobile-show search-bar-icon" href="#"><i class="fas fa-search"></i></a>
+						<a class="mobile-show search-bar-icon" href="{{ asset('#') }}"><i class="fas fa-search"></i></a>
 						<div class="mobile-menu"></div>
 						<!-- menu end -->
 					</div>
@@ -118,8 +118,8 @@
 								<p class="subtitle">Fresh & Organic</p>
 								<h1>Delicious Seasonal Fruits</h1>
 								<div class="hero-btns">
-									<a href="shop.html" class="boxed-btn">Fruit Collection</a>
-									<a href="contact.html" class="bordered-btn">Contact Us</a>
+									<a href="{{ asset('shop.html') }}" class="boxed-btn">Fruit Collection</a>
+									<a href="{{ asset('contact.html') }}" class="bordered-btn">Contact Us</a>
 								</div>
 							</div>
 						</div>
@@ -137,8 +137,8 @@
 								<p class="subtitle">Fresh Everyday</p>
 								<h1>100% Organic Collection</h1>
 								<div class="hero-btns">
-									<a href="shop.html" class="boxed-btn">Visit Shop</a>
-									<a href="contact.html" class="bordered-btn">Contact Us</a>
+									<a href="{{ asset('shop.html') }}" class="boxed-btn">Visit Shop</a>
+									<a href="{{ asset('contact.html') }}" class="bordered-btn">Contact Us</a>
 								</div>
 							</div>
 						</div>
@@ -156,8 +156,8 @@
 								<p class="subtitle">Mega Sale Going On!</p>
 								<h1>Get December Discount</h1>
 								<div class="hero-btns">
-									<a href="shop.html" class="boxed-btn">Visit Shop</a>
-									<a href="contact.html" class="bordered-btn">Contact Us</a>
+									<a href="{{ asset('shop.html') }}" class="boxed-btn">Visit Shop</a>
+									<a href="{{ asset('contact.html') }}" class="bordered-btn">Contact Us</a>
 								</div>
 							</div>
 						</div>
@@ -214,11 +214,11 @@
                 <div class="footer-box pages">
                     <h2 class="widget-title">Pages</h2>
                     <ul>
-                        <li><a href="index.html">Home</a></li>
-                        <li><a href="about.html">About</a></li>
-                        <li><a href="services.html">Shop</a></li>
-                        <li><a href="news.html">News</a></li>
-                        <li><a href="contact.html">Contact</a></li>
+                        <li><a href="{{ asset('index.html') }}">Home</a></li>
+                        <li><a href="{{ asset('about.html') }}">About</a></li>
+                        <li><a href="{{ asset('services.html') }}">Shop</a></li>
+                        <li><a href="{{ asset('news.html') }}">News</a></li>
+                        <li><a href="{{ asset('contact.html') }}">Contact</a></li>
                     </ul>
                 </div>
             </div>
@@ -263,25 +263,25 @@
 <!-- end copyright -->
 
 <!-- jquery -->
-<script src="assets/js/jquery-1.11.3.min.js"></script>
+<script src="{{ asset('assets/js/jquery-1.11.3.min.js') }}"></script>
 <!-- bootstrap -->
-<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="{{ asset('assets/bootstrap/js/bootstrap.min.js') }}"></script>
 <!-- count down -->
-<script src="assets/js/jquery.countdown.js"></script>
+<script src="{{ asset('assets/js/jquery.countdown.js') }}"></script>
 <!-- isotope -->
-<script src="assets/js/jquery.isotope-3.0.6.min.js"></script>
+<script src="{{ asset('assets/js/jquery.isotope-3.0.6.min.js') }}"></script>
 <!-- waypoints -->
-<script src="assets/js/waypoints.js"></script>
+<script src="{{ asset('assets/js/waypoints.js') }}"></script>
 <!-- owl carousel -->
-<script src="assets/js/owl.carousel.min.js"></script>
+<script src="{{ asset('assets/js/owl.carousel.min.js') }}"></script>
 <!-- magnific popup -->
-<script src="assets/js/jquery.magnific-popup.min.js"></script>
+<script src="{{ asset('assets/js/owl.carousel.min.js') }}"></script>
 <!-- mean menu -->
-<script src="assets/js/jquery.meanmenu.min.js"></script>
+<script src="{{ asset('assets/js/jquery.meanmenu.min.js') }}"></script>
 <!-- sticker js -->
-<script src="assets/js/sticker.js"></script>
+<script src="{{ asset('assets/js/sticker.js') }}"></script>
 <!-- main js -->
-<script src="assets/js/main.js"></script>
+<script src="{{ asset('assets/js/main.js') }}"></script>
 
 </body>
 </html>
